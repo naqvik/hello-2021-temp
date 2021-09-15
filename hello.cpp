e71d5729f2e4741c0179aec27cddecdfcd6499bd
Karim Naqvi <naqvik@uregina.ca>
2021-09-15 12:52:44 -0600

    Add a sonnet

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
+
+std::string shakespearian_diversion() {
+    std::string s =
+        "Shall I compare thee to a summer's day?\n"
+        "Thou art more lovely and more temperate\n"
+        "Rough winds to shake the darling buds of May\n"
+        "and Summer's lease hath all too short a date\n"
+        "Sometime too hot the eye of heaven shines\n"
+        "and often is his gold complexion dimm'd\n"
+        "and every fair from fair sometime declines\n"
+        "by chance or nature's changing course untrimm'd\n"
+        "But thy eternal summer shall not fade,\n"
+        "nor lose posession of that fair thow owe'st\n"
+        "Nor shall Death brag thou wanderest in his shade\n"
+        "when in eternal lines to time thou growest\n"
+        ;
+    return s;
+}
 
 int main() {
     using namespace std;
@@ -15,4 +34,6 @@ int main() {
         cout << setprecision(7);
         cout << x << "|" << sin(x) << "\n";
     }
+    cout << shakespearian_diversion();
+    cout << "...\nWhat's a good ending?\n\n";
 }
