6ca8e0fdd1784a66490533931bf6104135150de2
Karim Naqvi <naqvik@uregina.ca>
2021-09-15 12:30:21 -0600

    Now print a sine table
    Rationale: exercise some of the cout manipulators, and the math library.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 int main() {
     using namespace std;
 
     cout << "hello\n";
+    cout << "sine table\n"
+         <<" x (rad) | sin(x)\n"
+         <<"---------+-------\n";
+
+    for (int i=0; i<10; ++i) {
+        double x = i*0.01;
+        cout << setprecision(7);
+        cout << x << "|" << sin(x) << "\n";
+    }
 }
